b99494579f240c44eaa795861e44a03130ddb505
Ruslan <manoiloruslan@gmail.com>
2023-10-07 19:55:04 +0300

    Starting settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+
+class App extends Component {
+  state = {
+
+  };
+  
+  render() {
+    return (
+      <div>
+        React homework template
+      </div>
+    )
+  };
 };
+
+export default App;
